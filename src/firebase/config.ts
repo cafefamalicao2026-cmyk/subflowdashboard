706f34ada4fe1a2b90287b675ea333d087fc1c66
Hebert Alves <cafefamalicao2026@gmail.com>
2026-03-05 19:51:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4361258350-c96f8",
+  "appId": "1:787607201697:web:401adc5abc6f5aeaa18de0",
+  "apiKey": "AIzaSyC4O4cJ5WjTh4uoZojPtdgs5UUcX-9VYKI",
+  "authDomain": "studio-4361258350-c96f8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "787607201697"
+};
